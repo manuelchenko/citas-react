443ccfd7c7696c5cb7bb0bb4c79c06c7dc85410b
Manuelchenko <jcmanueldj@gmail.com>
2022-12-20 16:08:10 -0300

    Primer Commit

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,30 @@
+
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+      
+      <p className="font-bold mb-3 text-gray-700 uppercase">Propiestario: {''}
+        <span className="font-normal normal-case">Jose Manuel</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+        <span className="font-normal normal-case">josemanuel@gmail.com</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+        <span className="font-normal normal-case">10 Diciembre 2022</span>
+      </p>
+
+      <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+        <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam sunt cupiditate, consequuntur quaerat perferendis culpa soluta facere magni quo voluptates nemo necessitatibus magnam dolores iste eius consectetur quis quasi ipsa?</span>
+      </p>
+
+    </div>
+  )
+}
+
+export default Paciente
